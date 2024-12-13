cfc3458b2dadaf95d8e2b721fba93b30c90143fb
Lionel Tay <lioneltay@Lionels-Mac-mini.local>
2024-12-13 21:02:22 +1100

    basic setup

---
+++ demo/src/server.ts
@@ -0,0 +1,44 @@
+import express from "express"
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from "@apollo/server/express4"
+import cors from "cors"
+
+import { resolvers } from "./resolvers"
+import * as fs from "fs"
+import * as path from "path"
+import * as glob from "glob"
+
+// Combine all .gql files into a single schema
+const gqlFiles = glob.sync(path.join(__dirname, "schema", "*.gql"))
+const typeDefs = gqlFiles
+  .map((file) => fs.readFileSync(file, "utf8"))
+  .join("\n")
+
+async function startServer() {
+  const app = express()
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  })
+
+  // Note you must call `start()` on the `ApolloServer`
+  // instance before passing the instance to `expressMiddleware`
+  await server.start()
+
+  // Specify the path where we'd like to mount our server
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server),
+  )
+
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `🚀 Server ready at http://localhost:4000${server.graphqlPath}`,
+    ),
+  )
+}
+
+startServer().catch((err) => console.error(err))
